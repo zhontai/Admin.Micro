e027a6a2ef52517da07cb761584996e49bdac0bf
zhontai <361243234@qq.com>
2023-09-30 20:47:59 +0800

    修复登录异常问题

---
+++ src/03.Modules/Admin/ZhonTai.Module.Admin.HttpApi/Repositories/Log/LoginLogRepository.cs
@@ -0,0 +1,11 @@
+﻿using ZhonTai.Api.Core.Db.Transaction;
+using ZhonTai.Module.Admin.HttpApi.Domain.LoginLog;
+
+namespace ZhonTai.Module.Admin.HttpApi.Repositories;
+
+public class LoginLogRepository : AdminRepositoryBase<LoginLogEntity>, ILoginLogRepository
+{
+    public LoginLogRepository(UnitOfWorkManagerCloud uowm) : base(uowm)
+    {
+    }
+}
---
+++ src/03.Modules/Admin/ZhonTai.Module.Admin.HttpApi/Repositories/Log/OprationLogRepository.cs
@@ -0,0 +1,11 @@
+﻿using ZhonTai.Api.Core.Db.Transaction;
+using ZhonTai.Module.Admin.HttpApi.Domain.OprationLog;
+
+namespace ZhonTai.Module.Admin.HttpApi.Repositories;
+
+public class OprationLogRepository : AdminRepositoryBase<OprationLogEntity>, IOprationLogRepository
+{
+    public OprationLogRepository(UnitOfWorkManagerCloud uowm) : base(uowm)
+    {
+    }
+}
